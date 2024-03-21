f05fe03b08afe66bdb017474fc61a88ce70911f2
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-22 02:39:10 +0600

    update: login

--- app/(auth)/login/page.uc.tsx
+++ app/(auth)/login/page.uc.tsx
@@ -1,51 +1,12 @@
 'use client';
 
-import React, { Suspense } from 'react';
-
-import { useRouter, useSearchParams } from 'next/navigation';
-import { errorNotification } from '@/lib/notification';
+import { Suspense } from 'react';
 
 import LoginForm from '@/components/ui/form/login.form';
 
 import scss from '@/components/scss/page.module.scss';
 
-type ErrorCode = {
-  code: string;
-  message: string;
-};
-
-const errorCodes: ErrorCode[] = [
-  {
-    code: '401',
-    message: 'Пользователь не существует',
-  },
-  {
-    code: '403',
-    message: 'Пользователь был деактивирован',
-  },
-];
-
 export default function LoginClient() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const error = searchParams.get('error'); // 401 or 403
-
-  React.useEffect(() => {
-    const checkErrors = () => {
-      // if (error) {
-      //   const errorCode = errorCodes.find((item) => item.code === error);
-
-      //   router.replace('/login');
-
-      //   if (errorCode) {
-      //     errorNotification(errorCode.message);
-      //   }
-      // }
-    };
-
-    checkErrors();
-  }, [error]);
-
   return (
     <>
       <section className={scss.wrapper}>
--- components/ui/form/login.form.tsx
+++ components/ui/form/login.form.tsx
@@ -20,10 +20,27 @@ type FormData = {
   password: string;
 };
 
+type ErrorCode = {
+  code: string;
+  message: string;
+};
+
+const errorCodes: ErrorCode[] = [
+  {
+    code: '401',
+    message: 'Пользователь не существует',
+  },
+  {
+    code: '403',
+    message: 'Пользователь был деактивирован',
+  },
+];
+
 export default function LoginForm() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const next = decodeURIComponent(searchParams.get('next') ?? '/');
+  const error = searchParams.get('error'); // 401 or 403
 
   const nextUrl = next === '/' ? '/' : `?next=${next}`;
 
@@ -53,6 +70,22 @@ export default function LoginForm() {
       console.error(error);
     }
   };
+
+  React.useEffect(() => {
+    const checkErrors = () => {
+      if (error) {
+        const errorCode = errorCodes.find((item) => item.code === error);
+
+        router.replace('/login');
+
+        if (errorCode) {
+          errorNotification(errorCode.message);
+        }
+      }
+    };
+
+    checkErrors();
+  }, [error]);
 
   return (
     <>
