15a4b8e547082e68a48531d7ea8692a04af7fe6a
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-22 02:31:29 +0600

    update: login

--- app/(auth)/login/page.uc.tsx
+++ app/(auth)/login/page.uc.tsx
@@ -1,12 +1,47 @@
 'use client';
 
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { errorNotification } from '@/lib/notification';
 
 import LoginForm from '@/components/ui/form/login.form';
 
 import scss from '@/components/scss/page.module.scss';
 
+type ErrorCode = {
+  code: string;
+  message: string;
+};
+
+const errorCodes: ErrorCode[] = [
+  {
+    code: '401',
+    message: 'Пользователь не существует',
+  },
+  {
+    code: '403',
+    message: 'Пользователь был деактивирован',
+  },
+];
+
 export default function LoginClient() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const error = searchParams.get('error'); // 401 or 403
+
+  React.useEffect(() => {
+    if (error) {
+      const errorCode = errorCodes.find((item) => item.code === error);
+
+      router.replace('/login');
+
+      if (errorCode) {
+        errorNotification(errorCode.message);
+      }
+    }
+  }, [error]);
+
   return (
     <>
       <section className={scss.wrapper}>
