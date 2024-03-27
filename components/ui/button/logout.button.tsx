8cc78496e965759873bb0966b181fab1dbba4735
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-03-27 10:49:30 +0600

    update: logout

--- components/ui/button/logout.button.tsx
+++ components/ui/button/logout.button.tsx
@@ -1,14 +1,25 @@
 'use client';
 
+import React from 'react';
+
 import Button from './button';
 
 import { LogoutSvg } from '@/public/svg';
 
 export default function LogOutButton() {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  const handleLoading = () => {
+    setIsLoading(!isLoading);
+  };
+
   return (
     <Button
       redirect={`${process.env.NEXT_PUBLIC_API_URL}/logout`}
       style="logout"
+      load={isLoading}
+      disabled={isLoading}
+      onClick={handleLoading}
       icon={{
         svg: <LogoutSvg />,
         position: 'left',
